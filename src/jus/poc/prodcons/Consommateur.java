26397fa5461742abc7f8e4c401ca072f88ef0491
mnogueron <matthieu.nogueron@live.fr>
2015-12-06 18:52:41 +0100

    implement bad things

--- src/jus/poc/prodcons/Consommateur.java
+++ src/jus/poc/prodcons/Consommateur.java
@@ -4,4 +4,29 @@ package jus.poc.prodcons;
  * Created by matthieu on 06/12/15.
  */
 public class Consommateur extends Acteur implements _Consommateur {
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public int deviationTempsDeTraitement() {
+        return 0;
+    }
+
+    @Override
+    public int identification() {
+        return 0;
+    }
+
+    @Override
+    public int moyenneTempsDeTraitement() {
+        return 0;
+    }
+
+    @Override
+    public int nombreDeMessages() {
+        return 0;
+    }
 }
--- src/jus/poc/prodcons/Observateur.java
+++ src/jus/poc/prodcons/Observateur.java
@@ -4,4 +4,45 @@ package jus.poc.prodcons;
  * Created by matthieu on 06/12/15.
  */
 public class Observateur {
+
+    private boolean coherent;
+    private static String ControlClass;
+
+    private boolean operationnel;
+
+    public Observateur(){
+
+    }
+
+    public boolean coherent(){
+        return coherent;
+    }
+
+    public void consommationMessage(_Consommateur c, Message m, int tempsDeTraitement){
+
+    }
+
+    public void depotMessage(_Producteur p, Message m){
+
+    }
+
+    public void init(int nbproducteurs, int nbconsommateurs, int nbBuffers){
+
+    }
+
+    public void newConsommateur(_Consommateur c){
+
+    }
+
+    public void newProducteur(_Producteur p){
+
+    }
+
+    public void productionMessage(_Producteur p, Message m, int tempsDeTraitement){
+
+    }
+
+    public void retraitMessage(_Consommateur c, Message m){
+
+    }
 }
