baddac5493aa3f3d380f65e81d37e0c672328b4c
Reatha <rea.tith@gmail.com>
2015-12-09 22:13:51 +0100

    Implement methods in ProdCons

--- src/jus/poc/prodcons/ProdCons.java
+++ src/jus/poc/prodcons/ProdCons.java
@@ -4,28 +4,68 @@ package jus.poc.prodcons;
  * Created by matthieu on 06/12/15.
  */
 public class ProdCons implements Tampon {
+	
+	private Message[] buffer;
+	private int nbBusy;
+	private int in;
+	private int out ;
+	
+	/**
+	 * ProdCons constructor 
+	 * @param bufferSize
+	 */
+	public ProdCons(int bufferSize) {
+		nbBusy = 0;
+		in = 0;
+		out = 0;
+		buffer = new Message[bufferSize];
+		for(int i=0; i<taille(); i++){
+			buffer[i] = null;
+		}
+	}
 
 	@Override
 	public int enAttente() {
-		// TODO Auto-generated method stub
-		return 0;
+		for(int i=0; i<taille(); i++){
+			if (buffer[i] != null){
+				nbBusy++;
+			}
+		}
+		return nbBusy;
 	}
 
 	@Override
 	public Message get(_Consommateur c) {
-		// TODO Auto-generated method stub
-		return null;
+		while(nbBusy <= 0){
+			try {
+				c.wait();
+			} catch (InterruptedException e) {}
+		}
+		
+		Message m = buffer[out];
+		out = (out-1)%taille();
+		nbBusy--;
+		notify();
+			
+		return m;
 	}
 
 	@Override
 	public void put(_Producteur p, Message m) {
-		// TODO Auto-generated method stub
+		while(nbBusy >= taille()){
+			try {
+				p.wait();
+			} catch (InterruptedException e) {}
+		}
 		
+		buffer[in] = m;
+		in = (in+1)%taille();
+		nbBusy++;
+		notify();
 	}
 
 	@Override
 	public int taille() {
-		// TODO Auto-generated method stub
-		return 0;
+		return buffer.length;
 	}
 }
