9ab7a0cd0b87b58314f2efdd1032e7cdcaa57d62
Reatha <rea.tith@gmail.com>
2015-12-09 23:08:03 +0100

    Implement methods in Consommateur

--- src/jus/poc/prodcons/Consommateur.java
+++ src/jus/poc/prodcons/Consommateur.java
@@ -3,23 +3,45 @@ package jus.poc.prodcons;
 /**
  * Created by matthieu on 06/12/15.
  */
-public class Consommateur extends Acteur implements _Acteur, Runnable {
+public class Consommateur extends Acteur implements _Consommateur, Runnable {
 
-	// constructor
-	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement)
+	private int nbMessages;
+	private ProdCons prodCons;
+	private int nbMessagesToRead;
+	
+	/**
+	 * Constructor for Consommateur
+	 * @param type
+	 * @param observateur
+	 * @param moyenneTempsDeTraitement
+	 * @param deviationTempsDeTraitement
+	 * @param prodCons
+	 * @param moyenneNbExemplaires
+	 * @param deviationNbExemplaires
+	 * @throws ControlException
+	 */
+	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement,
+			ProdCons prodCons, int moyenneNbExemplaires, int deviationNbExemplaires)
 			throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.prodCons = prodCons;
+		// generate random number of messages that the Consommateur can read
+		nbMessagesToRead = Aleatoire.valeur(moyenneNbExemplaires, deviationNbExemplaires);
+		nbMessages = nbMessagesToRead;
 	}
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-
+    	Message m;
+    	while(nbMessages > 0){
+    		m = prodCons.get(this);
+    		nbMessages--;
+    	}
     }
 
+    // number of messages already processed by the Consommateur
 	@Override
 	public int nombreDeMessages() {
-		// TODO Auto-generated method stub
-		return 0;
+		return nbMessagesToRead-nbMessages;
 	}
 }
